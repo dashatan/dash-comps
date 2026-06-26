e3f3bc80856e72b71abe519ae0d1701aa4524407
dash atan <dashatanad@gmail.com>
2026-06-26 17:54:02 +0330

    feat: add formatting and linting scripts with husky integration

--- prettier.config.js
+++ prettier.config.js
@@ -2,4 +2,10 @@
 export default {
   plugins: ["prettier-plugin-tailwindcss"],
   tailwindStylesheet: "./packages/styles/index.css",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  printWidth: 80,
+  endOfLine: "lf",
 };
